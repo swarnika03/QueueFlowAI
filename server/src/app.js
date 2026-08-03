c7405b39fff3c125ba84e81abc12c3b0f63f6651
swarnika03 <pariswarnika@gmail.com>
2026-08-03 17:56:16 +0530

    Fix CORS for deployed frontend

--- server/src/app.js
+++ server/src/app.js
@@ -1,86 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.get("/", (req, res) => {
-//   res.json({
-//     message: "QueueFlow AI Backend Running",
-//   });
-// });
-
-// const PORT = 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-// import http from "http";
-// import { Server } from "socket.io";
-// import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import User from "./models/User.js";
-// import connectDB from "./config/db.js";
-// import authRoutes from "./routes/authRoutes.js";
-// import userRoutes from "./routes/userRoutes.js";
-// import adminRoutes from "./routes/adminRoutes.js";
-// import branchRoutes from "./routes/branchRoutes.js";
-// import tokenRoutes from "./routes/tokenRoutes.js";
-// dotenv.config();
-
-// connectDB();
-
-// const app = express();
-// const server = http.createServer(app);
-
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:5173",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//   },
-// });
-// app.use(cors());
-// app.use(express.json());
-
-// app.use(
-//   "/api/auth",
-//   authRoutes
-// );
-// app.use(
-//   "/api/users",
-//   userRoutes
-// );
-// app.use(
-//   "/api/admin",
-//   adminRoutes
-// );
-// app.use(
-//   "/api/branches",
-//   branchRoutes
-// );
-// app.use(
-//   "/api/tokens",
-//   tokenRoutes
-// );
-
-// app.get("/", (req, res) => {
-//   res.json({
-//     message: "QueueFlow AI Backend Running",
-//   });
-// });
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//   console.log(
-//     `Server running on port ${PORT}`
-//   );
-// });
-
 
 import express from "express";
 import cors from "cors";
@@ -108,8 +25,12 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+     origin: [
+      "http://localhost:5173",
+      "https://queueflow-ai-frontend.onrender.com",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   },
 });
 
@@ -127,7 +48,16 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://queueflow-ai-frontend.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
